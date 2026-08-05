87cd6d65f0a622b072f3423a29452cf9e9e74b96
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2026-08-04 19:56:38 -0500

    feat: header no longer empty
    Header html elements in place for future styling
    and functionality implementations

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,34 @@
 import { Link } from "react-router"
-
+import { Menu, Search, Plus,Bell} from "lucide-react"
+import defaultPfpPNG from "../assets/react.svg"
 
 export function Dashboard() {
     return (
         <>
             <header>
-            
+                <div>
+                    <Menu/>
+                </div>
+                <div>
+                    <div role="search">
+                        <label htmlFor="search-query">
+                            <Search></Search>
+                            <input type="search" name="search-query" id="search-query" placeholder="Search jobs, customers, etc..." />
+                        </label>
+                    </div>
+                    <div>
+                        <button>
+                            <Plus></Plus>
+                            <p>Create Job</p>
+                        </button>
+                        <button>
+                            <Bell></Bell>
+                        </button>
+                        <button>
+                            <img src={defaultPfpPNG} alt="" />
+                        </button>
+                    </div>
+                </div>
             </header>
             <div>
                 <nav>
