c4b3650f4b3055f392c828a3b4b892a06a9ca58f
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2026-08-04 19:30:19 -0500

    refactor: add basic semantic html organization
    Dashboard no longer empty in html but follow a
    basic organization to steer future ui.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,10 +4,16 @@ import { Link } from "react-router"
 export function Dashboard() {
     return (
         <>
-            <Link to="/">Dashboard</Link>
-            <Link to="work">Work</Link>
-            <Link to="notifications">Notifications</Link>
-            <Link to="settings">Settings</Link>
+            <header>
+            
+            </header>
+            <div>
+                <nav>
+
+                </nav>
+                <main>
+                </main>
+            </div>
         </>
     )
 }
