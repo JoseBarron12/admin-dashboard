89004c3077f574c2eb4457753993af25357d5747
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2026-08-04 21:19:58 -0500

    style: header no longer default styling + format
    Header easier to distinguish for user.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,30 +1,30 @@
 import { Link } from "react-router"
 import { Menu, Search, Plus,Bell} from "lucide-react"
 import defaultPfpPNG from "../assets/react.svg"
-
+import styles from "../style/Dashboard.module.css"
 export function Dashboard() {
     return (
         <>
-            <header>
+            <header className={styles.header}>
                 <div>
-                    <Menu/>
+                    <Menu className={styles.headerIcon}/>
                 </div>
-                <div>
+                <div className={styles.headerLeft}>
                     <div role="search">
-                        <label htmlFor="search-query">
-                            <Search></Search>
+                        <label htmlFor="search-query" className={styles.headerSearch}>
+                            <Search className={styles.headerIcon}></Search>
                             <input type="search" name="search-query" id="search-query" placeholder="Search jobs, customers, etc..." />
                         </label>
                     </div>
-                    <div>
-                        <button>
-                            <Plus></Plus>
+                    <div className={styles.headerBtns}>
+                        <button className={styles.headerJobBtn}>
+                            <Plus className={styles.headerIcon}></Plus>
                             <p>Create Job</p>
                         </button>
-                        <button>
-                            <Bell></Bell>
+                        <button className={styles.headerNotiBtn}>
+                            <Bell className={styles.headerIcon}></Bell>
                         </button>
-                        <button>
+                        <button className={styles.headerPfp}>
                             <img src={defaultPfpPNG} alt="" />
                         </button>
                     </div>
