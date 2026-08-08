5e14d6e0c65f6a15ac490215e9af265958191bea
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2026-08-08 16:35:12 -0500

    fix: header style no longer break on small screen
    Smaller user screens no longer cause header
    buttons + icons from breaking UI design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,36 @@
 import { Menu, Search, Plus, Bell} from "lucide-react"
 import styles from "../style/Header.module.css"
 import defaultPfpPNG from "../assets/react.svg"
+import { useEffect, useState} from "react"
+
 
 export function Header({showSideBar, setShowSideBar}) {
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        
+        const createBuffer = async () => {
+            const yuh = await new Promise(resolve => setTimeout(resolve, 1500));
+            console.log("PASSE")
+        }
+        
+        createBuffer()// artificially create 1s delay before changing screen width
+        
+        window.addEventListener("resize", () => {
+            setScreenWidth(window.innerWidth);
+        });
+
+        setScreenWidth(window.innerWidth);
+
+        return () => {
+            
+            window.removeEventListener("resize", () => {
+            setScreenWidth(window.innerWidth);
+            });
+        }
+    },[screenWidth]);
+    
+    
     return (
         <header className={showSideBar ? styles.headerSmall : styles.header}>        
             { !showSideBar && <button className={styles.navBtn} onClick={() => {
@@ -15,7 +43,8 @@ export function Header({showSideBar, setShowSideBar}) {
                 <div role="search">
                     <label htmlFor="search-query" className={styles.headerSearch}>
                         <Search className={styles.headerIcon}></Search>
-                        <input type="search" name="search-query" id="search-query" placeholder="Search jobs, customers, etc..." />
+                        <input type="search" name="search-query" id="search-query" 
+                        placeholder = {screenWidth <= 700 ? "" : screenWidth <= 1000 ? "Search" : "Search jobs, customers, etc..."} />
                     </label>
                 </div>
                 
@@ -26,7 +55,7 @@ export function Header({showSideBar, setShowSideBar}) {
                     </button>
                     
                     <button className={styles.headerNotiBtn}>
-                        <Bell className={styles.headerBigIcon}></Bell>
+                        <Bell className={screenWidth <= 700 ? styles.headerIcon : styles.headerBigIcon}></Bell>
                     </button>
                             
                     <button className={styles.headerPfp}>
